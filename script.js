95b7c1120c29ac5afe991718667377f732e8ccd8
AwaneeshSinghBisht <163861104+AwaneeshSinghBisht@users.noreply.github.com>
2026-09-25 21:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+    let cost = 0;
+
+    while (arr.length > 1) {
+
+        // Sort smallest to largest
+        arr.sort((a, b) => a - b);
+
+        // Take two smallest ropes
+        let first = arr.shift();
+        let second = arr.shift();
+
+        // Connect them
+        let sum = first + second;
+
+        // Add connection cost
+        cost += sum;
+
+        // Put new rope back
+        arr.push(sum);
+    }
+
+    return cost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
